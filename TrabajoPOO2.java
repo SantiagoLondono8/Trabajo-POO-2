dc64cbd245e9af94336ed3a36fec32a33d040a6d
Santiago <55521570+SantiagoLondono8@users.noreply.github.com>
2022-10-01 19:38:44 -0500

    Delete TrabajoPOO2.java

--- TrabajoPOO2.java
+++
@@ -1,9 +0,0 @@
-package trabajopoo2;
-public class TrabajoPOO2 {
-
-    public static void main(String[] args) {
-        Principal ventana = new Principal();
-        ventana.show(true);
-    }
-    
-}
